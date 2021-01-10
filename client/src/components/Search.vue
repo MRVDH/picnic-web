864828bf10b2bb91d7b881c3614887025393f28e
MRVDH <info@maartenvandenhoven.com>
2021-01-10 17:19:01 +0100

    Search category progress

--- client/src/components/Search.vue
+++ client/src/components/Search.vue
@@ -1,8 +1,8 @@
 <template>
     <div>
-        <b-row>
-            <b-col cols="12">
-                <b-input-group>
+        <b-list-group>
+            <b-list-group-item>
+                <b-input-group v-if="!selectedCategory">
                     <b-input-group-prepend is-text>
                         <b-icon icon="search" />
                     </b-input-group-prepend>
@@ -13,18 +13,81 @@
                         placeholder="Welk product zoek je?"
                         />
                 </b-input-group>
-            </b-col>
-        </b-row>
+                <div v-else>
+                    <b-icon
+                        icon="arrow-left-short"
+                        font-scale="1.5"
+                        @click="selectParent(selectedCategory.id)"
+                        />
+                    {{ selectedCategory.name }}
+                </div>
+            </b-list-group-item>
 
-        <div v-if="categories && categories.length">
-            <b-row
-                v-for="category in categories"
-                :key="category.id"
+            <div
+                v-if="!selectedCategory"
+                class="other-items"
                 >
-                <b-col cols="12">
+                <b-list-group-item
+                    v-for="category in allCategories"
+                    :key="category.id"
+                    button
+                    @click="selectCategory(category)"
+                    >
+                    <!-- future image placeholder -->
+                    <!-- <img src="../assets/img/placeholder-small.png"> -->
                     {{ category.name }}
-                </b-col>
-            </b-row>
+
+                    <b-icon
+                        class="float-right"
+                        icon="chevron-right"
+                        font-scale="0.8"
+                        />
+                </b-list-group-item>
+            </div>
+            <div v-if="selectedCategory && !showItems">
+                <b-list-group-item
+                    v-for="category in selectedCategory.items"
+                    :key="category.id"
+                    button
+                    @click="selectCategory(category)"
+                    >
+                    <!-- future image placeholder -->
+                    <!-- <img src="../assets/img/placeholder-small.png"> -->
+                    {{ category.name }}
+
+                    <b-icon
+                        class="float-right"
+                        icon="chevron-right"
+                        font-scale="0.8"
+                        />
+                </b-list-group-item>
+            </div>
+        </b-list-group>
+
+        <div v-if="showItems">
+            <div
+                v-for="category in selectCategoriesFromFinalCategory(selectedCategory.items)"
+                :key="category.id"
+                class="categories"
+                >
+                <b-row>
+                    <b-col cols="12">
+                        <span class="category-name">{{ category.name }}</span>
+                    </b-col>
+                </b-row>
+                <b-row>
+                    <b-col
+                        v-for="product in category.items"
+                        :key="product.id"
+                        cols="3"
+                        >
+                        <CustomProductCard
+                            v-if="product.type === 'SINGLE_ARTICLE'"
+                            :product="product"
+                            />
+                    </b-col>
+                </b-row>
+            </div>
         </div>
     </div>
 </template>
@@ -32,12 +95,19 @@
 <script>
 import ApiService from '@/services/ApiService';
 
+import CustomProductCard from '@/components/ProductCard';
+
 export default {
     name: 'Search',
+    components: {
+        CustomProductCard
+    },
     data () {
         return {
             searchText: "",
-            categories: []
+            allCategories: [],
+            selectedCategory: null,
+            showItems: false
         }
     },
     computed: {
@@ -66,25 +136,93 @@ export default {
     },
     methods: {
         getCategories () {
-            ApiService.getCategories(3).then(res => {
-                this.categories = res.data.filter(x => x.is_included_in_category_tree);
+            ApiService.getCategories().then(res => {
+                this.allCategories = res.data.filter(x => x.is_included_in_category_tree);
             });
+        },
+        selectCategory (category) {
+            this.selectedCategory = category;
+
+            this.showItems = category.items.length && category.items.find(x => x.type === "SINGLE_ARTICLE");
+
+            if (category.items.length) {
+                return;
+            }
+
+            ApiService.getList(category.id, null, 1).then((res) => {
+                this.selectedCategory.items = res.data;
+
+                this.showItems = this.selectedCategory.items.length && this.selectedCategory.items.find(x => x.type === "SINGLE_ARTICLE");
+            })
+        },
+        selectParent (parentId) {
+            for (let category of this.allCategories) {
+                for (let subCategory of category.items) {
+                    if (subCategory.id === parentId) {
+                        this.selectedCategory = category;
+                        return;
+                    }
+
+                    if (subCategory.items && subCategory.items.length) {
+                        for (let subSubCategory of subCategory.items) {
+                            if (subSubCategory.id === parentId) {
+                                this.selectedCategory = subCategory;
+                                return;
+                            }
+                        }
+                    }
+                }
+            }
+
+            this.selectedCategory = null;
+        },
+        selectCategoriesFromFinalCategory (items) {
+            return items.filter(x => x.type === "CATEGORY");
         }
     }
 }
 </script>
 
 <style scoped>
+.other-items .list-group-item:first-child {
+    border-top: none;
+}
+
 .input-group-text {
     border-right: none;
-    background: #fff;
+    background: #f8f8f8;
+    border-color: #eee;
     border-top-left-radius: 10px;
     border-bottom-left-radius: 10px;
 }
 
 .form-control {
     border-left: none;
+    background: #f8f8f8;
+    border-color: #eee;
     border-top-right-radius: 10px;
     border-bottom-right-radius: 10px;
+}
+
+img {
+    height: 10px;
+}
+
+.b-icon.bi-chevron-right {
+    margin-top: 5px;
+}
+
+.b-icon.bi-arrow-left-short {
+    cursor: pointer;
+    vertical-align: -0.25em;
+    margin-left: -6px;
+}
+
+.categories .row {
+    margin-bottom: 15px;
+}
+
+.category-name {
+    font-weight: 500;
 }
 </style>
