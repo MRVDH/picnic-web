60d1ce89751bffbb49dfd7bbd69e977df0fe47f4
MRVDH <info@maartenvandenhoven.com>
2021-02-03 20:19:37 +0100

    Scroll to top on tab and route change

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -32,7 +32,14 @@ const routes = [
 ];
 
 const router = new VueRouter({
-    routes
+    routes,
+    scrollBehavior (to, from, savedPosition) {
+        if (savedPosition) {
+            return savedPosition
+        } else {
+            return { x: 0, y: 0 }
+        }
+    }
 });
 
 export default router;
