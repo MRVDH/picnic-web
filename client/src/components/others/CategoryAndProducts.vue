f5aacea9128222580566712d25b243e4f9099e87
MRVDH <info@maartenvandenhoven.com>
2021-02-17 09:07:50 +0100

    Added show more buttons

--- client/src/components/others/CategoryAndProducts.vue
+++ client/src/components/others/CategoryAndProducts.vue
@@ -7,12 +7,33 @@
         </b-row>
         <b-row>
             <b-col
-                v-for="product in getProductsOnly(category.items)"
+                v-for="product in productsOnly"
                 :key="product.id"
                 cols="6"
                 md="3"
                 >
                 <CustomProductCard :product="product" />
+            </b-col>
+            <b-col
+                v-if="category.items.length && hasMore && !isShowingMore"
+                cols="6"
+                md="3"
+                >
+                <b-col
+                    cols="12"
+                    class="view-more"
+                    @click="showMore()"
+                    >
+                    <div class="inner-container">
+                        <span>
+                            Bekijk meer
+                        </span>
+                        <b-icon
+                            icon="chevron-right"
+                            font-scale="2.5"
+                            />
+                    </div>
+                </b-col>
             </b-col>
         </b-row>
         <b-row v-if="!category.items.length">
@@ -42,34 +63,73 @@ export default {
         category: Object,
         parentId: String
     },
-    mounted () {
-        if (this.category.decorators && this.category.decorators.length && this.category.decorators.find(x => x.type === "MORE_BUTTON")) {
+    data () {
+        return {
+            isShowingMore: false
+        }
+    },
+    computed: {
+        productsOnly () {
+            return this.category.items.filter(x => x.type === "SINGLE_ARTICLE");
+        },
+        hasMore () {
+            return this.category.decorators && this.category.decorators.length && this.category.decorators.find(x => x.type === "MORE_BUTTON");
+        }
+    },
+    methods: {
+        showMore () {
             if (this.parentId) {
                 ApiService.getList(this.parentId, this.category.id, 1).then((res) => {
                     this.category.items = res.data;
+                    this.isShowingMore = true;
                 });
             } else {
                 ApiService.getList(this.category.id, null, 1).then((res) => {
                     this.category.items = res.data;
+                    this.isShowingMore = true;
                 });
             }
-        }
-    },
-    methods: {
-        getProductsOnly (items) {
-            return items.filter(x => x.type === "SINGLE_ARTICLE");
         }
     }
 }
 </script>
 
-<style scoped>
+<style scoped lang="scss">
 .row {
     margin-bottom: 15px;
 }
 
 .category-name {
     font-weight: 500;
+}
+
+.view-more {
+    background: #fff;
+    border-radius: 4px;
+    height: 370px;
+    margin-bottom: 6px;
+
+    .inner-container {
+        height: 100%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-direction: column;
+        color: #E1171E;
+        cursor: pointer;
+
+        span {
+            font-weight: 600;
+            line-height: 4rem;
+        }
+
+        .b-icon {
+            padding: 10px;
+            color: white;
+            background: #E1171E;
+            border-radius: 50%;
+        }
+    }
 }
 
 .b-skeleton-img {
